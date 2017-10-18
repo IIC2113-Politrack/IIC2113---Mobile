05c6d1eb8a0c82db6ed52d6801ce504e2adbbe1d
sofizambrano <sizambrano@uc.cl>
2017-10-18 20:01:32 -0300

    Proposal, organization and politician pages done

--- src/pages/proposals/proposals.ts
+++ src/pages/proposals/proposals.ts
@@ -15,6 +15,18 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ProposalsPage {
 
+	public proposals = [
+  	{
+  		title: "Política Pública de Salud Mental Coordinada para la prevención del suicidio infantil y adolescente",
+  		num: 175,
+  		org:"Fundación Todo Mejora",
+  		org_pic:"https://votainteligente.cl/cache/cache/d4/e8/d4e8b67e3e9c184a1b21e120500b7ecb.png",
+  		content: "Articular los esfuerzos realizados desde el área de la salud con aquellos emanados desde el sistema escolar a fin de que las acciones tengan repercusiones para el % de los casos de NNA que no presentan depresión, pero si riesgo suicida.",
+  		tags: ["INFANCIA Y JUVENTUD", "SALUD"],
+  		upvotes: 12
+  	}
+  	]
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
