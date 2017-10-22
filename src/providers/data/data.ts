a162d5e70b5bc462b703d2c3d045b1ca5d6fcdc3
imarquezc <ignaciomarquezc@gmail.com>
2017-10-22 17:13:48 -0300

    main api endpoints

--- src/providers/data/data.ts
+++ src/providers/data/data.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Headers, RequestOptions } from '@angular/http';
 import 'rxjs/add/operator/map';
 
 @Injectable()
@@ -10,8 +10,11 @@ export class DataProvider {
   constructor(private _http: Http) { }
 
   public getUsers() {
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
   	console.log("getting users");
-    return this._http.get(this.apiUrl +  "users")
+    let options = new RequestOptions({ headers: headers, method: "get" });
+    return this._http.get(this.apiUrl +  "users", options)
       .map(response => response.json());
   }
 
